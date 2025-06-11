4945fdee0b1f1d86ba531909ca1b96388a34cf74
Jason Shao <Jason.Shao@dal.ca>
2025-06-11 17:06:37 -0300

    feat(js): extracted nav as its own html, create nav-loader.js to load it dynamically

---
+++ assets/js/nav-loader.js
@@ -0,0 +1,16 @@
+function loadHTML(selector, file) {
+    fetch(file)
+        .then(res => {
+            if (!res.ok) throw new Error(`Failed to load ${file}`);
+            return res.text();
+        })
+        .then(data => {
+            const container = document.querySelector(selector);
+            if (container) container.innerHTML = data;
+        })
+        .catch(err => console.error(err));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadHTML("#nav-container", "nav.html");
+});
